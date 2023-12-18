b010843f192fb45de6b895d84886a8e627b4f2a0
Francoiscl28 <122871360+Francoiscl28@users.noreply.github.com>
2023-12-18 13:03:35 +0100

    Arreglar tu cosa y meter un game manager en el que he empezado a hacer mi parte (persona2) y todo el funcionamiento de los enemigos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int Tiempo, Puntos;
+    [SerializeField] GameObject Texto, Boton, Personaje, Fuego, Mosca;
+    bool Cronometro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boton.SetActive(false);
+        Texto.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { if (Cronometro == true)
+        {
+
+        }
+        
+    }
+
+    void Perder()
+    {
+        Personaje.SetActive(false);
+        Fuego.SetActive(false);
+        Mosca.SetActive(false);
+        Texto.SetActive(true);
+        Boton.SetActive(true);
+        Cronometro = false;
+    }
+
+    void ReiniciarJuego()
+    {
+        Puntos = 0;
+        Personaje.SetActive(true);
+        Fuego.SetActive(true);
+        Mosca.SetActive(true);
+        Texto.SetActive(false);
+        Boton.SetActive(false);
+        Tiempo = 0;
+    }
+}
+
--- Assets/Scripts/MoverMosca.cs
+++ Assets/Scripts/MoverMosca.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class MoverMosca : MonoBehaviour
 {
     [SerializeField] float velocidad;
+    Camera camara;
+    Vector2 PInicial, PMinima;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        camara = Camera.main;
+        PMinima = camara.ViewportToWorldPoint(new Vector2(0, 0));
+        PInicial = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+
+        if (transform.position.x < PMinima.x-5) 
+        {
+            transform.position = PInicial;
+            velocidad++;
+        }
     }
 }
