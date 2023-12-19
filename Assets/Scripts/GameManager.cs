450e678048f3af258d5e62292ee3bb9debe1be8d
Lerkham <javiolisanju75@gmail.com>
2023-12-19 14:51:37 +0100

    Modificaciones GameManager y mas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    int Tiempo, Puntos;
+    public static GameManager Instancia;
+    [SerializeField] float tiempo;
+    [SerializeField] int Puntos;
+    [SerializeField] public int puntuacionActual, mejorPuntuacion;
     [SerializeField] GameObject Texto, Boton, Personaje, Fuego, Mosca;
-    bool Cronometro;
+    [SerializeField] TMP_Text texto;
+    [SerializeField] MoverPersonaje controladorPersonaje;
+    [SerializeField] bool Cronometro;
     [SerializeField] MoverMosca Enemigo;
-
+    private void Awake()
+    {
+        if (Instancia == null)
+        {
+            Instancia = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         Boton.SetActive(false);
         Texto.SetActive(false);
+        mejorPuntuacion = PlayerPrefs.GetInt("mejorPuntuacion");
     }
 
     // Update is called once per frame
     void Update()
-    { if (Cronometro == true)
+    {      
+        if (Cronometro == true)
         {
-
+            
+            tiempo -= Time.deltaTime;
+            int minutos = (int)tiempo / 60;
+            int segundos = (int)tiempo % 60;
+            Debug.Log(minutos + ":" + segundos);
+            string textoTiempo = $"{minutos:D2} : {segundos:D2}";
+            texto.text = textoTiempo;
         }
         
     }
@@ -43,9 +68,18 @@ public class GameManager : MonoBehaviour
         Mosca.SetActive(true);
         Texto.SetActive(false);
         Boton.SetActive(false);
-        Tiempo = 0;
+        tiempo = 0;
         Cronometro = true;
         Enemigo.ReiniciarEnemigo();
+    }
+    public void SumarPunto()
+    {
+        puntuacionActual += 1;
+        if (puntuacionActual >= mejorPuntuacion)
+        {
+            mejorPuntuacion = puntuacionActual;
+            PlayerPrefs.SetInt("mejorPuntuacion", mejorPuntuacion);
+        }
     }
 }
 
